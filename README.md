ab0e12101cf268210abdd1631e10af05b0c67a1f
mirar <mira.razaf@gmail.com>
2021-12-02 02:07:44 +0100

    Day 1 and .gitignore files added. Update of README file.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 C++ wannabe solutions for advent of code 2021 
 All the proposed algorithms you may find in this repo don't necessarily work or are the most efficient. 
 You can take them and do whatever you want, just remind yourself that reusing the portion from my code is at your own risk. 
+
+Every problem will have its solution in the folder corresponding to its day of release. For each day you can find: the C++ code, the application, a test file (corresponding to the problem short data) and my puzzle input. The input files are .txt files. 
---
+++ day01/day01.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string> 
+#include <fstream> 
+using namespace std;
+
+
+int sumArray(int n, int* array) { 
+  int sum = 0; 
+  for (int i = 0; i < n; ++i) {
+    sum += array[i]; 
+  }  
+  return sum; 
+}
+
+int puzzle1(string filename) {
+  ifstream file; 
+  file.open(filename); 
+  string filecontent; 
+  int counter = 0; 
+
+  if (file.is_open() ) { 
+    int old = 999999999; 
+
+    while (file.good()) { 
+      getline (file, filecontent); 
+      int curr = stoi(filecontent); 
+
+      if (old < curr) { 
+        ++counter; 
+      }
+
+      old = curr; 
+    }
+  }
+
+  file.close(); 
+  return counter; 
+}
+
+int puzzle2(string filename, int slidewidth) {
+  ifstream file; 
+  file.open(filename); 
+  int counter = 0; 
+  int slidecount = 0; 
+  string filecontent; 
+
+  int index = 0, oldsum = 0, currsum = 0; 
+  int terms[slidewidth]; 
+
+  if (file.is_open()) {
+    while (file.good()) { 
+      getline (file, filecontent); 
+      int idxcurr = index % slidewidth; 
+      terms[idxcurr] = stoi(filecontent); 
+      if (index >= slidewidth) {
+        int currsum = sumArray(slidewidth, terms); 
+
+        if (oldsum < currsum) { 
+          ++counter; 
+        }
+
+        oldsum = currsum; 
+      } 
+      ++index; 
+    } 
+  }
+
+  file.close(); 
+  return counter; 
+}
+
+
+int main() {
+  /* Instantiation of the file */ 
+  string myfilename; 
+  cout << "Filename of the input: "; 
+  cin >> myfilename; 
+
+  cout << "The solution to part one is " << puzzle1(myfilename) << ". \n"; 
+  cout << "The solution to part two is " << puzzle2(myfilename, 3) << ". \n"; 
+
+  return 0; 
+}
